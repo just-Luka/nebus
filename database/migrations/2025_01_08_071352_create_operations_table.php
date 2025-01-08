258d5135ded248a66d8912695b8d10a24ae48602
Luka T <l.tsiklauri@itmegainsight.com>
2025-01-08 11:45:34 +0400

    Project setup

---
+++ database/migrations/2025_01_08_071352_create_operations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->foreignId('parent_id')
+                ->nullable()
+                ->constrained('operations')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operations');
+    }
+};
